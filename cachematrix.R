e8e6f316534ef883f5a22c34369e70d38571d480
jannehele <jannehele@gmail.com>
2015-12-26 18:18:29 +0200

    first version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions implement matrix inverse calculation and the value caching
+## the makeCacheMatrix is used to store the value while the cachesolve is used to call the 
+## makeCacheMatrix function and to get the value from cache (if available)
 
-## Write a short comment describing this function
+## This first function creates a list of functions that can be called when needed
+## to obtain the inverse value of specific matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The next function is called with the argument x, so you first run the makeCacheMatrix and store the output to variable x
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+# example of using functions above, this is matrix that can be inversed:
+# mdat <- matrix(c(3,1, 4,2), nrow = 2, ncol = 2, byrow = TRUE)
+# x <- makeCacheMatrix(mdat)
+# cachesolve(x)
+# cachesolve(x)
+# the second cachesolve function call will produce output with cached data
